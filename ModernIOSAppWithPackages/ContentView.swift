6fd10620caf0a2c50c5fd7217a61f3ccf945fb8d
Uday Pandey <uday.pandey@crvsh.io>
2020-06-29 18:09:20 +0100

    Initial Commit

---
+++ ModernIOSAppWithPackages/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ModernIOSAppWithPackages
+//
+//  Created by Uday Pandey on 29/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ModernIOSAppWithPackages/ModernIOSAppWithPackagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernIOSAppWithPackagesApp.swift
+//  ModernIOSAppWithPackages
+//
+//  Created by Uday Pandey on 29/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct ModernIOSAppWithPackagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
